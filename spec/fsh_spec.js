f6414d4dd2614934b7b1ca6fd320662fb80a3265
Bob Remeika <bremeika@yammer-inc.com>
2011-05-18 20:47:53 -0700

    Added isFileSync

--- spec/fsh_spec.js
+++ spec/fsh_spec.js
@@ -1,7 +1,28 @@
-var foounit = require('foounit').globalize();
+var foounit = require('foounit').globalize()
+  , fsh = require('fsh')
+  , pth = require('path');
 
-describe('', function (){
-  xit('needs tests');
+var fixtures;
+
+before(function (){
+  fixtures = pth.join(__dirname, 'fixtures');
+});
+
+describe('isFileSync', function (){
+  describe('when the resource is a file', function (){
+    it('returns true', function (){
+      var file = pth.join(fixtures, 'dir/file.txt');
+      expect(fsh.isFileSync(file)).to(beTrue);
+    });
+  });
+
+  describe('when the resource is not a file', function (){
+    it('returns false', function (){
+      var dir = pth.join(fixtures, 'dir');
+      expect(fsh.isDirectorySync(dir)).to(beTrue);
+      expect(fsh.isFileSync(dir)).to(beFalse);
+    });
+  });
 });
 
 foounit.run();
